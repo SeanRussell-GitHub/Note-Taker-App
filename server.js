e056e91d936b4f01d7d40755d5564339f90c9f66
Sean Russell <emailseanrussell@gmail.com>
2021-08-26 18:30:12 -0700

    last try connecting heroku - port issues?

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const uuid = () => {
 }
 
 // Set up Express
-const PORT = process.env.PORT || 30089;
+const PORT = process.env.PORT || 80;
 const app = express();
 
 // Sets up the middleware to handle data parsing
