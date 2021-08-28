d66db98da27f3b4a342b601ea51fed068a4b5e06
Sean Russell <emailseanrussell@gmail.com>
2021-08-28 09:35:06 -0700

    heroku attempts

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const morgan = require('morgan');
 const app = express();
 const uuid = () => {
   return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
