99a3deb0746c4f398d4039b52d0d83789c827ce4
Sean Russell <emailseanrussell@gmail.com>
2021-08-27 22:52:36 -0700

    heroku work

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join('/public/notes.html'));
+  res.sendFile(path.join(__dirname + '/public/notes.html'));
 });
 
 app.get('/api/notes', (req, res) => {
@@ -61,7 +61,7 @@ app.delete('/api/notes/:id', (req, res) => {
 });
 
 app.get('/*', (req,res) =>
-  res.sendFile(path.join('/public/index.html')));
+  res.sendFile(path.join(__dirname + '/public/index.html')));
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
